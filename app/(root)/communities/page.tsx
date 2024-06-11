1e2daf48280dcc7eb1b90718fbf7febe3c572310
aarav.gupta <aarav.gupta@tothenew.com>
2024-06-11 17:25:34 +0530

    pushed code

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -3,10 +3,10 @@ import { redirect } from "next/navigation";
 
 import Searchbar from "@/components/shared/Searchbar";
 import Pagination from "@/components/shared/Pagination";
+import CommunityCard from "@/components/cards/CommunityCard";
 
 import { fetchUser } from "@/lib/actions/user.actions";
 import { fetchCommunities } from "@/lib/actions/community.actions";
-import CommunityCard from "@/components/cards/CommunityCard";
 
 async function Page({
   searchParams,
